657181e5500a41e7605f3d7a2975f0ded737fae6
Kaylan Thompson <kaylanthompson@icloud.com>
2022-10-28 13:44:02 -0500

    made it through new packages in juicbox part 2

---
+++ api/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const usersRouter = express.Router();
+
+usersRouter.use((req, res, next) => {
+  console.log("A request is being made to /users");
+
+  res.send({ message: 'hello from /users!' });
+});
+
+module.exports = usersRouter;
