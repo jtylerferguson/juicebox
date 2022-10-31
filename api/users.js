d768a810cf44174e47b2a719685533e041f8beba
Kaylan Thompson <kaylanthompson@icloud.com>
2022-10-31 10:40:00 -0500

    finished setting up the create user, part 2 of juicebox part2

--- api/users.js
+++ api/users.js
@@ -22,12 +22,9 @@ usersRouter.get('/', async (req, res) => {
 });
 
 const jwt = require('jsonwebtoken');
-const { getUserByUsername
-
-} = require('../db');
+const { getUserByUsername, createUser} = require('../db');
 const { JWT_SECRET } = process.env;
-// const token = jwt.sign({ id: 3, username: 'joshua' }, 'server secret');
-// const recoveredData = jwt.verify(token, 'server secret');
+
 usersRouter.post('/login', async (req, res, next) => {
   const { username, password } = req.body;
 
@@ -58,6 +55,42 @@ usersRouter.post('/login', async (req, res, next) => {
     console.log(error);
     next(error);
   }
+});
+
+usersRouter.post('/register', async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: 'UserExistsError',
+        message: 'A user by that username already exists'
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location,
+    });
+
+    const token = jwt.sign({ 
+      id: user.id, 
+      username
+    }, process.env.JWT_SECRET, {
+      expiresIn: '1w'
+    });
+
+    res.send({ 
+      message: "thank you for signing up",
+      token 
+    });
+  } catch ({ name, message }) {
+    next({ name, message })
+  } 
 });
 
 module.exports = usersRouter;
