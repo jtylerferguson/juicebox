fdc8bb3b70c1a0ec7d463555047a3bcde83fa60a
Kaylan Thompson <kaylanthompson@icloud.com>
2022-10-31 15:44:17 -0500

    accidentaly made a change in office hours and added vs code to gitignore

--- api/posts.js
+++ api/posts.js
@@ -1,53 +1,47 @@
-const express = require('express');
-const postsRouter = express.Router();
-const { getAllPosts, createPost, getPostById} = require('../db');
-const { requireUser } = require('./utils');
+const express = require("express")
+const postsRouter = express.Router()
+const { getAllPosts, createPost} = require("../db")
+const { requireUser } = require("./utils")
 
-postsRouter.post('/', requireUser, async (req, res, next) => {
-  const { title, content, tags = ""} = req.body;
-  console.log(req.user, "this is our user")
+postsRouter.post("/", requireUser, async (req, res, next) => {
+    const { title, content, tags = "" } = req.body
+    console.log(req.user, "this is our user")
 
-  const tagArr = tags.trim().split(/\s+/)
-  const postData = {authorId: req.user.id, title, content};
+    const tagArr = tags.trim().split(/\s+/)
+    const postData = { authorId: req.user.id, title, content }
 
-  // only send the tags if there are some to send
-  if (tagArr.length) {
-    postData.tags = tagArr;
-  }
-
-  try {
-
-    // add authorId, title, content to postData object
-    post = await createPost(postData)
-    // const post = await createPost(postData);
-    // this will create the post and the tags for us
-    // if the post comes back, res.send({ post });
-    if (post) {
-      res.send(post)
+    // only send the tags if there are some to send
+    if (tagArr.length) {
+        postData.tags = tagArr
     }
 
-  } catch ({ name, message }) {
-    next({ name, message });
-  }
-});
-
+    try {
+        // add authorId, title, content to postData object
+        post = await createPost(postData)
+        // const post = await createPost(postData);
+        // this will create the post and the tags for us
+        // if the post comes back, res.send({ post });
+        if (post) {
+            res.send(post)
+        }
+    } catch ({ name, message }) {
+        next({ name, message })
+    }
+})
 
 postsRouter.use((req, res, next) => {
-  console.log("A request is being made to /users");
+    console.log("A request is being made to /users")
 
-  next(); // THIS IS DIFFERENT
-});
-
-
-
+    next() // THIS IS DIFFERENT
+})
 
 // UPDATE
-postsRouter.get('/', async (req, res) => {
-  const posts = await getAllPosts();
+postsRouter.get("/", async (req, res) => {
+    const posts = await getAllPosts()
 
-  res.send({
-    posts
-  });
-});
+    res.send({
+        posts,
+    })
+})
 
 module.exports = postsRouter
