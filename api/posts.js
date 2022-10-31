57958ef9fe5d33802af2dadc3ef35ff67905ecce
Kaylan Thompson <kaylanthompson@icloud.com>
2022-10-31 13:43:38 -0500

    completed Writing Post/api/posts

--- api/posts.js
+++ api/posts.js
@@ -1,5 +1,36 @@
 const express = require('express');
 const postsRouter = express.Router();
+const { getAllPosts, createPost, getPostById} = require('../db');
+const { requireUser } = require('./utils');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = ""} = req.body;
+  console.log(req.user, "this is our user")
+
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {authorId: req.user.id, title, content};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+
+    // add authorId, title, content to postData object
+    post = await createPost(postData)
+    // const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    if (post) {
+      res.send(post)
+    }
+
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -7,9 +38,7 @@ postsRouter.use((req, res, next) => {
   next(); // THIS IS DIFFERENT
 });
 
-const { getAllPosts
 
-} = require('../db');
 
 
 // UPDATE
