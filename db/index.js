44ddb1129edbcac7f6f980101389fcd617fda320
krald0game@gmail.com$ ssh-keygen -t ed25519 -C krald0game@gmail.com <krald0game@gmail.com>
2022-11-01 12:48:26 -0500

    heroku setup

--- db/index.js
+++ db/index.js
@@ -1,6 +1,9 @@
 const { Client } = require('pg');
 
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client({
+    connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  });
 
 async function getAllUsers() {
     const { rows } = await client.query(
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const PORT = 3000
+const { PORT = 3000 } = process.env
 const express = require("express")
 const server = express()
 
