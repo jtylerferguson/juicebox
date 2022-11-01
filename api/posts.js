bcd7ce082e3661744032c40afd01d6a2db0a8ab4
krald0game@gmail.com$ ssh-keygen -t ed25519 -C krald0game@gmail.com <krald0game@gmail.com>
2022-11-01 09:17:31 -0500

    push

--- api/posts.js
+++ api/posts.js
@@ -1,33 +1,33 @@
 const express = require("express")
 const postsRouter = express.Router()
-const { getAllPosts, createPost} = require("../db")
+const { getAllPosts, createPost, getPostById, updatePost} = require("../db")
 const { requireUser } = require("./utils")
 
 postsRouter.post("/", requireUser, async (req, res, next) => {
     const { title, content, tags = "" } = req.body
-    console.log(req.user, "this is our user")
+  console.log(req.user, "this is our user")
 
-    const tagArr = tags.trim().split(/\s+/)
+  const tagArr = tags.trim().split(/\s+/)
     const postData = { authorId: req.user.id, title, content }
 
-    // only send the tags if there are some to send
-    if (tagArr.length) {
+  // only send the tags if there are some to send
+  if (tagArr.length) {
         postData.tags = tagArr
-    }
+  }
 
-    try {
-        // add authorId, title, content to postData object
-        post = await createPost(postData)
-        // const post = await createPost(postData);
-        // this will create the post and the tags for us
-        // if the post comes back, res.send({ post });
-        if (post) {
-            res.send(post)
-        }
-    } catch ({ name, message }) {
+  try {
+    // add authorId, title, content to postData object
+    post = await createPost(postData)
+    // const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    if (post) {
+      res.send(post)
+    }
+  } catch ({ name, message }) {
         next({ name, message })
     }
-})
+        })
 
 postsRouter.use((req, res, next) => {
     console.log("A request is being made to /users")
@@ -35,13 +35,52 @@ postsRouter.use((req, res, next) => {
     next() // THIS IS DIFFERENT
 })
 
+
+
 // UPDATE
 postsRouter.get("/", async (req, res) => {
     const posts = await getAllPosts()
 
-    res.send({
+  res.send({
         posts,
     })
 })
+
+
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+    const { postId } = req.params;
+    const { title, content, tags } = req.body;
+  
+    const updateFields = {};
+  
+    if (tags && tags.length > 0) {
+      updateFields.tags = tags.trim().split(/\s+/);
+    }
+  
+    if (title) {
+      updateFields.title = title;
+    }
+  
+    if (content) {
+      updateFields.content = content;
+    }
+  
+    try {
+      const originalPost = await getPostById(postId);
+  
+      if (originalPost.author.id === req.user.id) {
+        const updatedPost = await updatePost(postId, updateFields);
+        res.send({ post: updatedPost })
+      } else {
+        next({
+          name: 'UnauthorizedUserError',
+          message: 'You cannot update a post that is not yours'
+        })
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  });
+
 
 module.exports = postsRouter
